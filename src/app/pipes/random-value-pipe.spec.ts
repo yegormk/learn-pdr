c4c4964600a620b1b466fa860f62602d0eba3687
Yehor Petrov <yehorpetrov5@gmail.com>
2026-02-25 00:25:32 +0000

    feat: mvp with basic test regime

---
+++ src/app/pipes/random-value-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RandomValuePipe } from './random-value-pipe';
+
+describe('RandomValuePipe', () => {
+  it('create an instance', () => {
+    const pipe = new RandomValuePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
